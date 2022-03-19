72bdb7db2625d24dca7aedb251c18a2f9959f1d0
Or Novogroder <or.novog@gmail.com>
2022-03-19 16:36:21 +0200

    remove unused function

--- Implementations/GetColumn.go
+++ Implementations/GetColumn.go
@@ -7,19 +7,6 @@ type GetColumn struct {
 	column int
 }
 
-func (g *GetColumn) Run() prc.LazyTable {
-	go func() {
-		defer close(g.OutputTable)
-		for row := range g.InputTable {
-			if len(row) > g.column {
-				g.OutputTable <- prc.Row{row[g.column]}
-			}
-		}
-	}()
-
-	return g.OutputTable
-}
-
 func NewGetColumn(column int) prc.Operation {
 	getColumn := func(row prc.Row, _ int) *prc.Row{
 		if len(row) > column{
